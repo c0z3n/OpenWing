cc6cfbb7d9a3696ef775bbf5c4fa50c424909cb1
c0z3n <c0z3n@gerblonix.org>
2013-03-29 13:39:18 -0400

    reworking how control data is sent from the device

--- src/OpenWing/OpenWing.ino
+++ src/OpenWing/OpenWing.ino
@@ -1,22 +1,3 @@
-// OpenWing.ino - arduino sketch which comprises the firmware of the OpenWing device
-// full repository located at github.com/c0z3n/OpenWing
-// contact gordon.p.clement@gmail.com
-//
-//     Copyright (C) Gordon Clement 2012
-//
-//     This program is free software: you can redistribute it and/or modify
-//     it under the terms of the GNU General Public License as published by
-//     the Free Software Foundation, either version 3 of the License, or
-//     (at your option) any later version.
-//
-//     This program is distributed in the hope that it will be useful,
-//     but WITHOUT ANY WARRANTY; without even the implied warranty of
-//     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-//     GNU General Public License for more details.
-//
-//     You should have received a copy of the GNU General Public License
-//     along with this program.  If not, see http://www.gnu.org/licenses.
-
 #include <SPI.h>
 #include <Ethernet.h>
 
@@ -40,16 +21,17 @@ int  fader_raws[]          = {0, 0, 0, 0, 0, 0, 0, 0};
 byte fader_vals[]          = {0, 0, 0, 0, 0, 0, 0, 0};
 byte last_fader_vals[]     = {0, 0, 0, 0, 0, 0, 0, 0};
 
-// variables to keep track of the trackball between "frames"
-byte trackXp               = 0;
-byte trackYp               = 0;
-byte trackXm               = 0;
-byte trackYm               = 0;
+byte fader_flags[]         = {0, 0, 0, 0, 0, 0, 0, 0};
+byte button_flags[]        = {0, 0, 0, 0};
+
+byte fader_IDs[]           = {1, 2, 3, 4, 5, 6, 7, 8};
+byte button_IDs[]          = {9, 10, 11, 12};
+
 
 void setup(){
   analogReference(EXTERNAL);
-  Serial2.begin(9600);
-  Serial.begin(9600);
+//  Serial2.begin(9600);
+  Serial.begin(115200);
   for(int i=0; i<4; i++){
     for(int n=0; n<3; n++){
       pinMode(colorpins[i][n], OUTPUT);
@@ -64,99 +46,90 @@ void setup(){
     fader_raws[i] = analogRead(faders[i]);
     fader_vals[i] = last_fader_vals[i] = fader_raws[i] >> 2;
   }
-  attachInterrupt(2, trackLeft, CHANGE);
-  attachInterrupt(3, trackRight, CHANGE);
-  attachInterrupt(4, trackUp, CHANGE);
-  attachInterrupt(5, trackDown, CHANGE);
+//  attachInterrupt(2, trackLeft, CHANGE);
+//  attachInterrupt(3, trackRight, CHANGE);
+//  attachInterrupt(4, trackUp, CHANGE);
+//  attachInterrupt(5, trackDown, CHANGE);
 }
 
 void loop(){
   for(int i=0; i<4; i++){
   // check the state of each button
+    lastbuttons[i] = button_states[i];
     if(digitalRead(buttons[i]) == LOW){
       button_states[i] = 1;
     }
     else{
       button_states[i] = 0;
     }
+    if(button_states[i] != lastbuttons[i]){
+      button_flags[i] = 1;
+    }
+    else{
+      button_flags[i] = 0;
+    }
   }
   // check the fader states and map them to the correct range
   for(int i=0; i<8; i++){
+    fader_flags[i] = 0;
     fader_raws[i] = analogRead(faders[i]);
+    last_fader_vals[i] = fader_vals[i];
     fader_vals[i] = fader_raws[i] >> 2;
-  }
-  // read incoming serial messages
-  if(Serial2.available() > 1){
-    byte c1 = Serial2.read();
-    byte c2 = Serial2.read();
-    // check for the alignment key
-    if(c1 == 'l' && c2 == 'x'){
-      byte lednum = Serial2.read();
-      byte r = Serial2.read();
-      byte g = Serial2.read();
-      byte b = Serial2.read();
-      colorvals[lednum][0] = int(r);
-      colorvals[lednum][1] = int(g);
-      colorvals[lednum][2] = int(b);
+    if (fader_vals[i] == last_fader_vals[i]){
+      fader_flags[i] = 0;
     }
     else{
-      while(Serial2.available() > 0){
-        byte temp = Serial2.read();
-      }
-      Serial2.flush();
+      fader_flags[i] = 1;
     }
   }
-  set_colors();
+  // read incoming serial messages
+//  if(Serial2.available() > 1){
+//    byte c1 = Serial2.read();
+//    byte c2 = Serial2.read();
+//    // check for the alignment key
+//    if(c1 == 'l' && c2 == 'x'){
+//      byte lednum = Serial2.read();
+//      byte r = Serial2.read();
+//      byte g = Serial2.read();
+//      byte b = Serial2.read();
+//      colorvals[lednum][0] = int(r);
+//      colorvals[lednum][1] = int(g);
+//      colorvals[lednum][2] = int(b);
+//    }
+//    else{
+//      while(Serial2.available() > 0){
+//        byte temp = Serial2.read();
+//      }
+//      Serial2.flush();
+//    }
+//  }
+//  set_colors();
   send_data();
   delay(25);
 }
 
-void set_colors(){
-// handle setting led colors
-  for(int i=0; i<4; i++){
-    for(int n=0; n<3; n++){
-      if(button_states[i] == 1){
-        // if a button is pressed, override the color with white.
-        analogWrite(colorpins[i][0], 0);
-        analogWrite(colorpins[i][1], 0);
-        analogWrite(colorpins[i][2], 0);
+void send_data(){
+  byte sent_start = 0;
+  for(int i=0; i<8; i++){
+    if(fader_flags[i] == 1){
+      if(sent_start == 0){
+        Serial.write(sent_start);
+        Serial.write(sent_start);
+        sent_start = 1;
       }
-      else{
-        // else use the color as defined in colorvals
-        analogWrite(colorpins[i][0], colorvals[i][0]);
-        analogWrite(colorpins[i][1], colorvals[i][1]);
-        analogWrite(colorpins[i][2], colorvals[i][2]);
-      }
+      Serial.write(fader_IDs[i]);
+      Serial.write(fader_vals[i]);
     }
   }
-}
-
-void send_data(){
-  // send the states of all buttons and faders over serial
-  Serial2.write("dx");
   for(int i=0; i<4; i++){
-    Serial2.write(button_states[i]);
+    if(button_flags[i] == 1){
+      if(sent_start == 0){
+        Serial.write(sent_start);
+        Serial.write(sent_start);
+        sent_start = 1;
+      }
+      Serial.write(button_IDs[i]);
+      Serial.write(button_states[i]);
+    }      
   }
-  for(int i=0; i<8; i++){
-    Serial2.write(fader_vals[i]);
-  }
-  Serial2.write(trackXp);
-  Serial2.write(trackYp);
-  Serial2.write(trackXm);
-  Serial2.write(trackYm);
-  trackXm = trackYm = trackXp = trackYp = 0;
-  
-}
-
-void trackUp(){
-  trackYp = trackYp + 1;
-}
-void trackDown(){
-  trackYm = trackYm + 1;
-}
-void trackLeft(){
-  trackXp = trackXp + 1;
-}
-void trackRight(){
-  trackXm = trackXm + 1;
 }
